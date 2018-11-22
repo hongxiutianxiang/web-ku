51c16d06a27e51bd4f7de60df1bf0d87a99f898c
yueyuejun <981562544@qq.com>
2018-11-22 21:01:22 +0800

    js-外部-渲染

---
+++ 03-js/05js/for-005.js
@@ -0,0 +1,6 @@
+function tochange(){
+			var oDiv = document.getElementById('box1')
+			oDiv.style.width="200px";
+			oDiv.style.height="200px";
+			oDiv.style.backgroundColor="yellow";
+		}
---
+++ 03-js/05js/for-006.js
@@ -0,0 +1,8 @@
+function tochange(){
+			var oDiv = document.getElementById('box1')
+			oDiv.style.width="200px";
+			oDiv.style.height="200px";
+			oDiv.style.backgroundColor="yellow";
+		}
+		var oDiv =document.getElementById('box1')
+		oDiv.onclick = tochange
