8cfa41fbcab18ae2e06092d408b519958624e205
yueyuejun <981562544@qq.com>
2019-01-03 11:41:01 +0800

    单例模式练习

--- project/chuizi-SmartOs/js/SmartOS.js
+++ project/chuizi-SmartOs/js/SmartOS.js
@@ -13,16 +13,16 @@ var oTop3 = document.querySelector('.top .top3');
 window.onscroll = function(){
 	if(getScrollTop() >= 100){
 		oTop1.style.height = 0;
-		oTop2.style.position = 'fixed';
-		oTop3.style.position = 'fixed';
+		oTop2.style.top = 0;
+		oTop3.style.height = 40 + 'px';
+		oTop3.style.top = 40 + 'px';
+		// oTop2.style.position = 'fixed';
+
 	}else{
 		oTop1.style.height= 100 + 'px';
-		oTop2.style.position = 'relative';
-		oTop3.style.position = 'relative';
+		// oTop1.style.borderBottom = '1px solid #2f3351';
+		oTop3.style.height = 0;
+		oTop3.style.borderBottom = 'none';
+		oTop2.style.top = 100 + 'px';
 	}
-	if(oTop1.style.height = 0){
-		
-	}
-	
-	
 }
