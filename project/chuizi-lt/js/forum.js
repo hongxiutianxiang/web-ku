edae6c601ba69be2ebee0f1ce77799a09d1ea00f
yueyuejun <981562544@qq.com>
2019-01-13 20:47:13 +0800

    brand

--- project/chuizi-lt/js/forum.js
+++ project/chuizi-lt/js/forum.js
@@ -4,6 +4,7 @@ handCarousel();
 firstPlus();
 secondPlus();
 brandChose();
+typeChose();
 
 function handleForumList(){
 	var oPlate = document.getElementById('plate');
@@ -118,34 +119,166 @@ function secondPlus(){
 function brandChose(){
 	var oBrand = document.querySelector('.brand');
 	var oBrandChoseBtn = document.querySelector('.brand .brand-chose');
-	var oBrandContent = document.querySelector('.brand .brand-content');
+	var oBrandContent = document.querySelectorAll('.brand .brand-content');
 	var oTtiangle = document.querySelector('.col2 .slect .triangle');
 	var oBrandItem = document.querySelectorAll('.col2 .brand .brand-item');
+	var oFont1 = document.querySelectorAll('.brand .brand-chose span')[1];
+	var oFont0 = document.querySelectorAll('.brand .brand-chose span')[0];
+	var oMask0 = document.querySelectorAll('.main .content .col2 .mask')[0];
+	var oMask1 = document.querySelectorAll('.main .content .col2 .mask')[1];
+	var oMask2 = document.querySelectorAll('.main .content .col2 .mask')[2];
 
 	oBrandChoseBtn.onclick = function(){
 		oBrand.style.borderBottomLeftRadius = '0';
 		oBrand.style.borderBottomRightRadius = '0';
 
-		if(oBrandContent.style.display == 'block'){
-			oBrandContent.style.display = 'none';
+		function back(){
+			oBrandContent[0].style.display = 'none';
 			oTtiangle.className = 'triangle triangle-down';	
+			oBrand.style.borderBottomLeftRadius = '7px';
+			oBrand.style.borderBottomRightRadius = '7px';
+			oMask0.style.display = 'none';
+		}
+
+		if(oBrandContent[0].style.display == 'block'){
+			back();
 		}else{
-			oBrandContent.style.display = 'block';
+			oBrandContent[0].style.display = 'block';
 			oTtiangle.className = 'triangle triangle-up';
 		}
 		
 		for(var i = 0;i<oBrandItem.length;i++){
-			// oBrandItem[i].index = i;
+			oBrandItem[i].index = i;
 			oBrandItem[i].onmouseover = function(){
 				for(var j = 0;j<oBrandItem.length;j++){
 					oBrandItem[j].style.backgroundColor = '';
 				}
-				this.style.backgroundColor = '#f5f5f5';
-				
+				this.style.backgroundColor = '#f5f5f5';	
 			}
+			oBrandItem[0].onclick = function(){
+				oFont0.innerHTML = '手机品牌： 小米';
+				back();
+			}
+			oBrandItem[1].onclick = function(){
+				oFont0.innerHTML = '手机品牌： 一加';
+				back();
+			}
+			oBrandItem[2].onclick = function(){
+				oFont0.innerHTML = '手机品牌： Nexus';
+				back();
+			}
+			oBrandItem[3].onclick = function(){
+				oFont0.innerHTML = '手机品牌： 三星';
+				back();
+			}			
+		}
+		 if(oFont1.innerHTML == '请选择机型'){
+		 	
+		 }else{
+		 	oFont1.innerHTML = '请选择机型';
+		 }
+
+		oMask1.style.height = '100%';
+		oMask2.style.height = '100%';
+
+		
+		// if(oBrandContent[1].style.display == 'block'||
+		// 	oBrandContent[2].style.display == 'block'||
+		// 	oBrandContent[3].style.display == 'block'||
+		// 	oBrandContent[4].style.display == 'block'){
+		// 	oBrandContent[1].style.display = 'none';
+		// 	oBrandContent[2].style.display = 'none';
+		// 	oBrandContent[3].style.display = 'none';
+		// 	oBrandContent[4].style.display = 'none';
+
+		// 	if(oFont0.innerHTML == '手机品牌： 小米'){
+		// 		oBrandContent[1].style.display = 'block';
+		// 		oTtiangle.className = 'triangle triangle-down';
+		// 	}
+		// }
+	}	
+}
+
+
+function typeChose(){
+	var oBrand = document.querySelectorAll('.brand')[1];
+	var oBrandChoseBtn = document.querySelectorAll('.brand .brand-chose')[1];
+	var oBrandContent = document.querySelectorAll('.brand .brand-content');
+	var oTtiangle = document.querySelectorAll('.col2 .slect .triangle')[1];
+	var oBrandItem = document.querySelectorAll('.col2 .brand-type .brand-item');
+	var oFont1 = document.querySelectorAll('.brand .brand-chose span')[1];
+	var oFont0 = document.querySelectorAll('.brand .brand-chose span')[0];
+	var oMask1 = document.querySelectorAll('.brand .brand-chose span')[1];
+	var oMask2 = document.querySelectorAll('.main .content .col2 .mask')[2];
+	
+
+
+	oBrandChoseBtn.onclick = function(){
+		oBrand.style.borderBottomLeftRadius = '0';
+		oBrand.style.borderBottomRightRadius = '0';
+
+		function back(){
+			for(var k = 0;k<oBrandContent.length;k++){
+				oBrandContent[k].style.display = 'none';
+			}
+			// oBrandContent[1].style.display = 'none';
+			oTtiangle.className = 'triangle triangle-down';	
+			oBrand.style.borderBottomLeftRadius = '7px';
+			oBrand.style.borderBottomRightRadius = '7px';
+			// oMask1.style.display = 'none';
+			// oMask2.style.display = 'none';
+			oMask1.style.height = 0;
+			oMask2.style.height = 0;
+		}
+
+		
+		for(var i = 0;i<oBrandItem.length;i++){
+			oBrandItem[i].index = i;
+			oBrandItem[i].onmouseover = function(){
+				for(var j = 0;j<oBrandItem.length;j++){
+					oBrandItem[j].style.backgroundColor = '';
+				}
+				this.style.backgroundColor = '#f5f5f5';	
+			}
+			oBrandItem[i].onclick = function(){
+				oFont1.innerHTML = this.innerHTML;				
+				back();
+			}
+
+
+			if(oBrandContent[1].style.display == 'block'){
+				back();
+			}else if(oFont0.innerHTML == '手机品牌： 小米'){
+				oBrandContent[1].style.display = 'block';
+				oTtiangle.className = 'triangle triangle-up';
+			}else{
+				//oMask1.style.cursor = ' not-allowed';
+
+			}
+
+			if(oBrandContent[2].style.display == 'block'){
+				back();
+			}else if(oFont0.innerHTML == '手机品牌： 一加'){
+				oBrandContent[2].style.display = 'block';
+				oTtiangle.className = 'triangle triangle-up';
+			}
+
+			if(oBrandContent[3].style.display == 'block'){
+				back();
+			}else if(oFont0.innerHTML == '手机品牌： Nexus'){
+				oBrandContent[3].style.display = 'block';
+				oTtiangle.className = 'triangle triangle-up';
+			}
+
+			if(oBrandContent[4].style.display == 'block'){
+				back();
+			}else if(oFont0.innerHTML == '手机品牌： 三星'){
+				oBrandContent[4].style.display = 'block';
+				oTtiangle.className = 'triangle triangle-up';
+			}
+			
 		}
 	}
 }
-
 
 
