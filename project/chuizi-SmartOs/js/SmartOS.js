29130e82cd19ebc7cd4da9402cd8221a99e32dfa
yueyuejun <981562544@qq.com>
2019-01-14 12:02:50 +0800

    SmartOs-video

--- project/chuizi-SmartOs/js/SmartOS.js
+++ project/chuizi-SmartOs/js/SmartOS.js
@@ -5,24 +5,27 @@ function getScrollLeft(){
 	return window.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft;
 }
 
+hadleScroll();
 
-var oTop1 = document.querySelector('.top .top1');
-var oTop2 = document.querySelector('.top .top2');
-var oTop3 = document.querySelector('.top .top3');
+function hadleScroll(){
+	var oTop1 = document.querySelector('.top .top1');
+	var oTop2 = document.querySelector('.top .top2');
+	var oTop3 = document.querySelector('.top .top3');
 
-window.onscroll = function(){
-	if(getScrollTop() >= 100){
-		oTop1.style.height = 0;
-		oTop2.style.top = 0;
-		oTop3.style.height = 40 + 'px';
-		oTop3.style.top = 40 + 'px';
-		// oTop2.style.position = 'fixed';
+	window.onscroll = function(){
+		if(getScrollTop() >= 100){
+			oTop1.style.height = 0;
+			oTop2.style.top = 0;
+			oTop3.style.height = 40 + 'px';
+			oTop3.style.top = 40 + 'px';
+			// oTop2.style.position = 'fixed';
 
-	}else{
-		oTop1.style.height= 100 + 'px';
-		// oTop1.style.borderBottom = '1px solid #2f3351';
-		oTop3.style.height = 0;
-		oTop3.style.borderBottom = 'none';
-		oTop2.style.top = 100 + 'px';
+		}else{
+			oTop1.style.height= 100 + 'px';
+			// oTop1.style.borderBottom = '1px solid #2f3351';
+			oTop3.style.height = 0;
+			oTop3.style.borderBottom = 'none';
+			oTop2.style.top = 100 + 'px';
+		}
 	}
 }
