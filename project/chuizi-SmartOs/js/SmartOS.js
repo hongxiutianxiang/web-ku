ea5ed79ceea214c6aa574c7d5cb91a4d73a4403f
yueyuejun <981562544@qq.com>
2019-01-15 11:37:41 +0800

    car

--- project/chuizi-SmartOs/js/SmartOS.js
+++ project/chuizi-SmartOs/js/SmartOS.js
@@ -6,6 +6,9 @@ function getScrollLeft(){
 }
 
 hadleScroll();
+handCarousel();
+handleCar();
+
 
 function hadleScroll(){
 	var oTop1 = document.querySelector('.top .top1');
@@ -13,7 +16,7 @@ function hadleScroll(){
 	var oTop3 = document.querySelector('.top .top3');
 
 	window.onscroll = function(){
-		if(getScrollTop() >= 100){
+		if(getScrollTop() >=100){
 			oTop1.style.height = 0;
 			oTop2.style.top = 0;
 			oTop3.style.height = 40 + 'px';
@@ -27,5 +30,50 @@ function hadleScroll(){
 			oTop3.style.borderBottom = 'none';
 			oTop2.style.top = 100 + 'px';
 		}
+	}
+}
+
+
+function handCarousel(){
+	new Carousel({
+		id:'carousel1',
+		img:['images/shurufa1.jpg',
+			'images/shurufa2.jpg',
+			'images/shurufa3.jpg',
+			],
+		width:282,
+		height:568,
+		playDuration:3000,
+	});
+
+	new Carousel({
+		id:'carousel2',
+		img:['images/gengduoshengji1-1.jpg',
+			'images/gengduoshengji1-2.jpg',
+			'images/gengduoshengji1-3.jpg',
+			],
+		width:282,
+		height:568,
+		playDuration:3000,
+	});
+
+	new Carousel({
+		id:'carousel3',
+		img:['images/gengduoshengji3-1.png',
+			'images/gengduoshengji3-2.jpg',
+			],
+		width:282,
+		height:568,
+		playDuration:3000,
+	});
+}
+
+
+function handleCar(){
+	var oCar = document.querySelector('.top1 .content .top1-shop .car')
+	var oCarList = document.querySelector('.shop-list');
+	
+	oCar.onmouseenter = function(){
+		oCarList.style.display = 'block';
 	}
 }
