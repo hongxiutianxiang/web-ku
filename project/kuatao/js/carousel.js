f40b210c9f5f73a3badbaef36508f7f17ffa6938
yueyuejun <981562544@qq.com>
2019-03-04 21:02:57 +0800

    carousel

--- project/kuatao/js/carousel.js
+++ project/kuatao/js/carousel.js
@@ -29,13 +29,13 @@ Carousel.prototype = {
 			//监听事件
 			this.$elem
 			.hover(function(){//显示隐藏左右按钮
-				this.$controlBtns.show();
+				// this.$controlBtns.show();
 			}.bind(this),function(){
-				this.$controlBtns.hide();
+				// this.$controlBtns.hide();
 			}.bind(this))
 			.on('click','.control-left',function(){
-
-			})
+				this._fade(this.now-1)
+			}.bind(this))
 			.on('click','.control-right',function(){
 				this._fade(this.now+1)
 			}.bind(this))
@@ -44,9 +44,9 @@ Carousel.prototype = {
 	_fade:function(index){
 		//index 代表即将显示的
 		//隐藏当前的
-		this.$carouselItem.eq(this.now).showhide('hide');
+		this.$carouselItem.eq(this.now).showHide('hide');
 		//显示即将显示的
-		this.$carouselItem.eq(index).showhide('show');
+		this.$carouselItem.eq(index).showHide('show');
 		//处理底部指示按钮
 		this.$btns.eq(this.now).removeClass('active');
 		this.$btns.eq(index).removeClass('active');
