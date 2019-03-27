fc84fd76ff2a72a2a0f31251195e0bd61ebd0cab
yueyuejun <981562544@qq.com>
2019-03-27 15:29:30 +0800

    question

--- 03-senior/02-node-js/07-http/08-wish-mvc/01-server.js
+++ 03-senior/02-node-js/07-http/08-wish-mvc/01-server.js
@@ -10,12 +10,42 @@ const mime = require('./mime.json');
 const { getAll,add,remove } = require('./WishModel.js')
 
 const server = http.createServer((req,res)=>{
-	// console.log('url:::',req.url);
+	console.log('url:::',req.url);
 
 	let reqUrl = url.parse(req.url,true);
-	// console.log(reqUrl);
+	console.log(reqUrl);
 	let pathname = reqUrl.pathname;
+	console.log('pathname:::',reqUrl);
+	//约定：
+	//1.请求以/static/开始的路径是静态资源
+	//2.对于路由请求的约定:  /Controller/action/arg1/arg2...
+	//						/wish/add
+	//						/wish/del/12365
+	//             			/wish/index
 
+	if(pathname.startsWith('/static/')){//静态处理资源
+		let filePath = path.normalize(__dirname+ /static/ + pathname);
+		// console.log(filePath);//D:\web-ku\05-senior\02-node-js\07-http\06-blog\static\index.html
+		let extname = path.extname(filePath);
+		// console.log(extname);
+
+		fs.readFile(filePath,(err,data)=>{
+			if(err){
+				res.setHeader('Content-Type','text/html;charset=utf-8');
+				res.end('<h1>出错了aa</h1>');
+			}else{
+				res.setHeader('Content-Type',mime[extname]+';charset=utf-8');
+				res.end(data);			
+			}
+		});		
+	}else{//路由处理
+		let paths = pathname.split('/');
+		console.log(paths);
+		res.end('ok');
+	}
+
+
+/*
 	if(pathname == '/' || pathname == '/index.html'){//获取首页
 		getAll()
 		.then(data=>{
@@ -93,13 +123,7 @@ const server = http.createServer((req,res)=>{
 			}
 		})
 	}
-
-
-
-
-
-
-
+*/
 
 
 
