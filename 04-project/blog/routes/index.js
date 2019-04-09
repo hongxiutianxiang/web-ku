d9377b3fc9c92c80d90c31511918ff5fc9847f07
yueyuejun <981562544@qq.com>
2019-04-09 08:37:23 +0800

    pro

--- 04-project/blog/routes/index.js
+++ 04-project/blog/routes/index.js
@@ -1,9 +1,24 @@
 const express = require('express')
 const CategoryModel = require('../models/category.js')
+const ArticleModel = require('../models/article.js')
 
-const router = express.Router();
+const router = express.Router()
+
+async function getCommonData(req){
+	const categoriesPromise = CategoryModel.find({},'name').sort({order:-1});
+	const pageArticlesPromise = ArticleModel.getPaginationArticles(req);
+
+	const categories = await categoriesPromise;
+	const pageArticles = await pageArticlesPromise;
+
+	return {
+		categories,
+		pageArticles
+	}
+}
 
 router.get("/",(req,res)=>{
+	/**/
 	CategoryModel.find({},'name')
 	.sort({order:-1})
 	.then(categories=>{
@@ -13,6 +28,16 @@ router.get("/",(req,res)=>{
 			categories
 		})	
 	})
+	
+	/*
+	getCommonData(req)
+	.then(data=>{
+		const {categories,pageArticles} = data;
+		res.render('main/index',{
+			userInfo:req.userInfo,
+			categories		
+	})
+	*/
 })
 
 module.exports = router
