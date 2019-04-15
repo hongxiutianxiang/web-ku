a76fb591936e5128f61c2f91673f548dd2b6d1b7
yueyuejun <981562544@qq.com>
2019-04-15 21:08:46 +0800

    qwer

---
+++ 04-project/kmall1/admin/src/App.js
@@ -0,0 +1,28 @@
+
+import React,{ Component,Fragment } from 'react'
+import { BrowserRouter as Router,Route } from 'react-router-dom'
+
+import Login from './pages/login'  //index.js
+import Home from './pages/home'
+
+import './App.css'
+
+
+
+class App extends Component{
+	
+	render(){
+		
+		return( 	
+			<Router>
+				<div className="App">
+					<Route exact path="/" component={Home} />
+					<Route path="/login" component={Login} />
+				</div>
+			</Router>
+		)
+	}
+}
+
+
+export default App;
---
+++ 04-project/kmall1/admin/src/pages/home/index.js
@@ -0,0 +1,21 @@
+/*
+* @Author: TomChen
+* @Date:   2019-04-09 19:29:30
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-04-12 20:54:27
+*/
+
+import React,{ Component } from 'react'
+
+import './index.css'
+
+class Home extends Component {
+  render(){
+    return (
+      <h1>sdfsf</h1>
+    )
+  }
+}
+
+
+export default Home
